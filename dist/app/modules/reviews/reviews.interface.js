e2f0d451cf2ef5d230b5d82bace96ad69a9e6cd0
@aLaroc01 <laroc.graphics@gmail.com>
2026-09-01 12:23:43 -0400

    Remove stale dist, force rebuild

--- dist/app/modules/reviews/reviews.interface.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// export interface ServiceResponse<T = any> {
-//   status: boolean;
-//   message: string;
-//   data?: T;
-// }
